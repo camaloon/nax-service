43ee1cec912c8121c3310fab45cb1105bd3c42ea
Fernando Gessler <nando@camaloon.com>
2014-04-29 16:52:51 +0200

    Generics version of NaxServicesContainer.GetInstance<T>()

--- NaxIntegration/NaxServicesContainer.cs
+++ NaxIntegration/NaxServicesContainer.cs
@@ -1,20 +1,108 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
 
-/// <summary>
-/// Container for Nax objects
-/// </summary>
-public class NaxServicesContainer
+using System.Text;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+using a3ERPActiveX;
+
+namespace NaxIntegration
 {
-    static string DB_NAME = "pruebabd";
-
-    Enlace naxEnlace = new Enlace();
-    Factura naxFactura = new Factura();
-    Varios naxVarios = new Varios();
-
-    public NaxServicesContainer()
+    /// <summary>
+    /// Container for Nax objects
+    /// </summary>
+    public class NaxServicesContainer
     {
-        //naxEnlace.Iniciar(DB_NAME);
-        //ThrowExceptionIfError(1);
-    }
+        static string DB_NAME = "pruebabd";
 
+        Dictionary<Type, object> naxObjects = new Dictionary<Type, object>();
+
+        public NaxServicesContainer()
+        {
+            Enlace naxEnlace = GetInstance<Enlace>();
+            naxEnlace.Iniciar(DB_NAME);
+            ThrowExceptionIfError(1);
+        }
+
+        public T GetInstance<T>()
+        {
+            Type interfaceType = typeof(T);
+
+            if (interfaceType.Namespace != "a3ERPActiveX")
+            {
+                throw new ApplicationException("Trying to instantiate a non a3ERPActiveX class!!");
+            }
+
+            if (!naxObjects.ContainsKey(interfaceType))
+            {
+                string progId = interfaceType.ToString(); // TODO: is this allways correct?
+                Type classType = Type.GetTypeFromProgID(progId);
+                naxObjects.Add(interfaceType, Activator.CreateInstance(classType));
+            }
+            return (T)naxObjects[interfaceType];
+        }
+
+        public dynamic GetInstance(string className, out Type type)
+        {
+            string progId = "a3ERPActiveX." + className;
+            type = Type.GetTypeFromProgID(progId);
+            Debug.WriteLine("Searching for progId: " + progId);
+            if (!naxObjects.ContainsKey(type))
+            {
+                Debug.WriteLine("ProgId not found in cache. Creating...");
+                object instance = Activator.CreateInstance(type);
+                type = instance.GetType();
+                naxObjects.Add(type, instance);
+            }
+            Debug.WriteLine("New cache count: " + naxObjects.Count().ToString());
+            return naxObjects[type];
+        }
+
+        public void ThrowExceptionIfError(UInt16 code)
+        {
+            Enlace naxEnlace = GetInstance<Enlace>();
+            if (naxEnlace.bError)
+            {
+                throw new ApplicationException(string.Format("[NAX_ERROR {0}: {1}]", code, naxEnlace.sMensaje));
+            }
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        public static string SearchRegistry(string dllName)
+        {
+            RegistryKey clsidKey = Registry.ClassesRoot.OpenSubKey("CLSID");
+            foreach (string subKey in clsidKey.GetSubKeyNames()) {
+                RegistryKey inProcServer32Key = Registry.ClassesRoot.OpenSubKey("CLSID\\" + subKey + "\\InProcServer32");
+                if (inProcServer32Key != null)
+                {
+                    string valueName = (from value in inProcServer32Key.GetValueNames() where string.IsNullOrEmpty(value) select value).FirstOrDefault();
+                    string dllPathCandidate = GetLongPathName(inProcServer32Key.GetValue(valueName).ToString());
+                    if (dllPathCandidate.EndsWith("\\" + dllName))
+                    {
+                        Debug.WriteLine("Dll found: " + dllPathCandidate);
+                        return dllPathCandidate;
+                    }
+                }
+            }
+            throw new ApplicationException(string.Format("Dll {0} not found.", dllName));
+        }
+
+        public static String GetLongPathName(String shortPath)
+        {
+            StringBuilder longPath = new StringBuilder(1024);
+            if (0 == GetLongPathName(shortPath, longPath, longPath.Capacity))
+            {
+                return shortPath;
+            }
+            return longPath.ToString();
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern Int32 GetLongPathName(String shortPath, StringBuilder longPath, Int32 longPathLength);
+
+    }
 }
--- NaxIntegration/WebService.svc.cs
+++ NaxIntegration/WebService.svc.cs
@@ -7,6 +7,9 @@ using System.ServiceModel.Web;
 using System.Text;
 using System.Diagnostics;
 
+using System.Reflection;
+using System.Runtime.InteropServices;
+
 using a3ERPActiveX;
 
 namespace NaxIntegration
@@ -15,88 +18,69 @@ namespace NaxIntegration
         SessionMode = SessionMode.NotAllowed
     )]
     [ServiceBehavior(
-        ConcurrencyMode = ConcurrencyMode.Single,           // The nax dll doesn't looks like threadsafe
+        // The nax dll doesn't looks like bening threadsafe
+        ConcurrencyMode = ConcurrencyMode.Single,
         InstanceContextMode = InstanceContextMode.Single
     )]
     public class WebService
     {
-        static string DB_NAME = "pruebabd";
-
-        Enlace naxEnlace = new Enlace();
-        Maestro naxMaestro = new Maestro();
-        Factura naxFactura = new Factura();
-        Varios naxVarios = new Varios();
-
-        public WebService()
-        {
-            Debug.WriteLine("in WebService() constructor");
-
-            naxEnlace.Iniciar(DB_NAME);
-            ThrowExceptionIfError(1);
-        }
-
-        protected void ThrowExceptionIfError(UInt16 code)
-        {
-            if (naxEnlace.bError)
-            {
-                throw new ApplicationException(string.Format("[NAX_ERROR {0}: {1}]", code, naxEnlace.sMensaje));
-            }
-        }
-
-        [OperationContract]
-        public decimal TestFactura()
-        {
-            Debug.WriteLine("in NewFactura()");
-
-            naxFactura.Iniciar();
-            ThrowExceptionIfError(1);
-
-            naxFactura.Nuevo("30/03/2014", // Fecha de la factura
-                "1", // Código del cliente
-                false, // Indica que es de venta (True indicaría que es de compra)
-                false, // Indica que es de gestión (True indicaría que es contable)
-                true, // Indica que se desean las repercusiones contables
-                true); // Indica que se desean los vencimientos
-            ThrowExceptionIfError(2);
-
-            naxFactura.NuevaLineaArt("1", 3D);
-            naxFactura.AsFloatLin["PrcMoneda"] = 1500F;
-            naxFactura.AnadirLinea();
-            ThrowExceptionIfError(3);
-
-            decimal numFactura = naxFactura.Anade();
-            ThrowExceptionIfError(4);
-            
-            naxFactura.Acabar();
-            ThrowExceptionIfError(5);
-            
-            return numFactura;
-        }
+        NaxServicesContainer naxContainer = new NaxServicesContainer();
 
         [OperationContract]
         public string TestVariosQuery()
         {
-            Debug.WriteLine("in TestVariosQuery()");
-
+            dynamic naxVarios = naxContainer.GetInstance<Varios>();
             var result = naxVarios.CuentaArtV("1", "1");
-            ThrowExceptionIfError(1);
+            naxContainer.ThrowExceptionIfError(1);
 
             return result.ToString();
         }
 
         [OperationContract]
+        public decimal NewFactura()
+        {
+            dynamic naxFactura = naxContainer.GetInstance<Factura>();
+            
+            naxFactura.Iniciar();
+            naxContainer.ThrowExceptionIfError(1);
+
+            naxFactura.Nuevo(
+                "30/03/2014",   // Fecha de la factura
+                "1",            // Código del cliente
+                false,          // Indica que es de venta (True indicaría que es de compra)
+                false,          // Indica que es de gestión (True indicaría que es contable)
+                true,           // Indica que se desean las repercusiones contables
+                true            // Indica que se desean los vencimientos
+            );
+            naxContainer.ThrowExceptionIfError(2);
+
+            naxFactura.NuevaLineaArt("1", 3D);
+            naxFactura.AsFloatLin["PrcMoneda"] = 1500F;
+            naxFactura.AnadirLinea();
+            naxContainer.ThrowExceptionIfError(3);
+
+            decimal numFactura = naxFactura.Anade();
+            naxContainer.ThrowExceptionIfError(4);
+            
+            naxFactura.Acabar();
+            naxContainer.ThrowExceptionIfError(5);
+            
+            return numFactura;
+        }
+
+        [OperationContract]
         public void TestCrearCliente()
         {
-            Debug.WriteLine("in TestCrearCliente()");
+            dynamic naxMaestro = naxContainer.GetInstance<Maestro>();
 
             naxMaestro.Iniciar("Clientes");
             naxMaestro.Nuevo();
-            ThrowExceptionIfError(1);
-            naxMaestro.AsInteger["codcli"] = 24;
+            naxContainer.ThrowExceptionIfError(1);
+            naxMaestro.AsInteger["codcli"] = 25;
             naxMaestro.AsString["nomcli"] = "test cliente";
-            ThrowExceptionIfError(2);
+            naxContainer.ThrowExceptionIfError(2);
             naxMaestro.Guarda(false);
-            ThrowExceptionIfError(3);
+            naxContainer.ThrowExceptionIfError(3);
         }
 
     }
